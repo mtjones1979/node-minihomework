f1a8638bcb9bca9561bcd4b3173089e6ad11c628
riraq <riraq@mail.usf.edu>
2021-02-27 13:10:59 -0600

    added writefile function

--- index.js
+++ index.js
@@ -62,7 +62,25 @@ inquirer
         },
     ])
     .then((response) => {
-        console.log(response);
-        fs.appendFile('log.txt', `${response.name}\n`, (err) =>
-            err ? console.error(err) : console.log('Commit logged!'));
-    })
+      fs.writeFile('index.html', `
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Template Literals</title>
+</head>
+<body>
+<div id="music">${response.name}</div>
+<div id="music">${response.town}</div>
+<div id="music">${response.github}</div>
+<div id="music">${response.linkedin}</div>
+
+<script type="text/javascript" src="index.js"></script>
+</body>
+</html>        
+      `, (err) =>
+          err ? console.error(err) : console.log('Commit logged!'));
+  })
